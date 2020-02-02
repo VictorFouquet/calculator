998ba5bdfc4e0afb25d175c343fce12173646ef6
victor.fouquet <victor.fouquet@gmail.com>
2020-02-02 14:32:21 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Calculator from './components/calculator/Calculator'
+import Signature from './components/signature/Calculator'
 function App() {
   return (
     <div className="App">
       <Calculator />
+      <Signature />
     </div>
   );
 }
