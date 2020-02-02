c2fcf6022d2b577e492ce605eea0e6ecfe4ac6e0
victor.fouquet <victor.fouquet@gmail.com>
2020-02-02 14:38:07 +0100

    including signature

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Calculator from './components/calculator/Calculator'
-import Signature from './components/signature/Calculator'
+import Signature from './components/signature/Signature'
 function App() {
   return (
     <div className="App">
